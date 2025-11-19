0a1ea9377a88f5ccd5fce223c996b3876add6466
Ethan Mitchell <ethan.mitchell799@gmail.com>
2025-11-19 11:39:47 +0900

    fix the crashing error again, it was deleted from mia's commit

--- frontend/src/components/Filebar.tsx
+++ frontend/src/components/Filebar.tsx
@@ -407,6 +407,18 @@ const Filebar: React.FC<FilebarProps> = ({
 				accept='image/*'
 				style={{ display: 'none' }}
 				onChange={(e) => {
+					//this code snippet is necessary to prevent crashes when no images are passed in i.e. file->open->cancel
+					console.log("Image input changed");
+					if(!e.currentTarget.files || e.currentTarget.files.length === 0) {
+						console.log("File selection cancelled");
+						return;
+					}
+					console.log(e.currentTarget.files);
+					if (e.currentTarget.files) {
+						setImageFiles(e.currentTarget.files);
+					}
+					//end of code snippet
+					
 					const files = Array.from(e.currentTarget.files || []);
 					// keep only images (file.type may be empty for some files from folders — fallback to extension)
 					const imageFiles = files.filter(f =>
